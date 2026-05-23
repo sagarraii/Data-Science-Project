fbf333170b2e638de41d3aec7f02e84de5367ce3
SAGA dev <sagarrai9547@gmail.com>
2026-05-23 14:09:05 +0530

    feat: define/updated project folder structure

--- project_schema.py
+++ project_schema.py
@@ -13,6 +13,8 @@ list_of_files = [
     ".github/workflows/.gitkeep",
 
     f"src/{project_name}/__init__.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/exception.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
