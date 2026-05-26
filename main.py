0bd6452e98fe9d36ab9f17f32beb9397fb1a6f86
SAGA dev <sagarrai9547@gmail.com>
2026-05-26 23:51:51 +0530

    refactor: improve main pipeline execution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience.logging.logger import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -9,3 +10,14 @@ try:
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
 except Exception as e:
         raise 
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        data_validation_pipeline = DataValidationTrainingPipeline()
+        data_validation_pipeline.initiate_data_validation()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
+except Exception as e:
+        logger.info(e)
+        raise e
