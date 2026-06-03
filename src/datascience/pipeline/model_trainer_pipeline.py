1fabd00f978084cdb510d7d52541d9d7282fb3cc
SAGA dev <sagarrai9547@gmail.com>
2026-06-03 17:59:03 +0530

    feat: create model trainer pipelineline.py

---
+++ src/datascience/pipeline/model_trainer_pipeline.py
@@ -0,0 +1,18 @@
+from src.datascience.config.configuration import ConfigurationManager
+from src.datascience.components.model_trainer import ModelTrainer
+from src.datascience.logging.logger import logger
+
+STAGE_NAME = "Model Trainer Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_trainer(Self):
+        try:
+            config = ConfigurationManager()
+            model_trainer_config = config.get_model_trainer_config()
+            model_trainer = ModelTrainer(config=model_trainer_config)
+            model_trainer.train()
+        except Exception as e:
+            raise e
