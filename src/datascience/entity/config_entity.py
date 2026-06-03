9c6cae41c2f758e9fb02cf0a9c3ddfcc45f24775
SAGA dev <sagarrai9547@gmail.com>
2026-06-03 17:57:12 +0530

    feat: add model trainer config entity

--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -14,3 +14,18 @@ class DataValidationConfig:
     unzipped_data_dir: Path
     STATUS_FILE: str
     all_schema: dict
+
+@dataclass
+class DataTransformationConfig:
+    root_dir:Path
+    data_path:Path
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float
+    l1_ratio: float
+    target_column: str
